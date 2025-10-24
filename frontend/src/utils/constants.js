6b8e4a9e08658989ebb1e7362a0d9834c904c43e
nitrox1 <manaymehtalws@gmail.com>
2025-10-24 14:05:56 +0530

    health check trial 2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "/api"
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "/api"
+//export const BASE_URL = "http://localhost:8000"
 
 export const NOTE_TYPES = {
     TEXT: 'text',
